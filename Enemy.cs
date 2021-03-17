b4fc0762e64af225f15385f97cadaf9dbb836ac7
sue-qui <80835702+sue-qui@users.noreply.github.com>
2021-03-17 22:27:35 +0800

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float hp = 10;
+
+    internal Animator ani;
+    internal MonsterWander mw;
+    internal AnimatorStateInfo state;
+
+    void Awake()
+    {
+        mw = GetComponent<MonsterWander>();
+        ani = GetComponent<Animator>();
+    }
+
+    public virtual void StateMachine() { }
+
+    public virtual void Damage(float dmg)
+    {
+        hp -= dmg;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
